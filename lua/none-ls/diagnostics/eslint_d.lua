857c1f3b6c45a553863282482dceacbeba2e2d48
Zephyr Lykos <self@mochaa.ws>
2024-02-25 14:14:56 +0800

    Initial commit

---
+++ lua/none-ls/diagnostics/eslint_d.lua
@@ -0,0 +1,11 @@
+return require("none-ls.builtins.diagnostics.eslint").with({
+    name = "eslint_d",
+    meta = {
+        url = "https://github.com/mantoni/eslint_d.js/",
+        description = "Like ESLint, but faster.",
+        notes = {
+            "Once spawned, the server will continue to run in the background. This is normal and not related to null-ls. You can stop it by running `eslint_d stop` from the command line.",
+        },
+    },
+    command = "eslint_d",
+})
