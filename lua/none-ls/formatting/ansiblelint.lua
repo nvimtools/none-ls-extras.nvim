1437fccf52b8ce06c4925f9e6c8c3bb22836bba3
Stephen A. Davis <23553256+webdavis@users.noreply.github.com>
2025-10-31 16:43:31 -0600

    feat(formatting): add support for `ansible-lint --fix`

---
+++ lua/none-ls/formatting/ansiblelint.lua
@@ -0,0 +1,29 @@
+local h = require("null-ls.helpers")
+local methods = require("null-ls.methods")
+
+local FORMATTING = methods.internal.FORMATTING
+
+return h.make_builtin({
+    name = "ansiblelint",
+    meta = {
+        url = "https://ansible.readthedocs.io/projects/lint/rules/yaml/",
+        description = "Ansible style formatting using ansible-lint --fix",
+        notes = {
+            "Ansible-lint autofix provides autofixing capabilities for issues identified by ansible-lint via the ansible-lint --fix option. This command can automatically reformat YAML files and apply transforms for specific rules to fix or simplify identified issues.",
+        },
+    },
+    method = FORMATTING,
+    filetypes = { "yaml.ansible" },
+    generator_opts = {
+        command = "ansible-lint",
+        args = { "--fix" },
+        check_exit_code = function(code)
+            return code == 0 or code == 2
+        end,
+        on_output = function(_)
+            vim.cmd("checktime")
+            return nil
+        end,
+    },
+    factory = h.formatter_factory,
+})
